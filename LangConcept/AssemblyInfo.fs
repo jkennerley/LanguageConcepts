0cc1b4f8dcc30c44cfba02e57502cfc7881ae27a
johnkennerley <john.kennerley@gmail.com>
2016-02-21 12:50:00 +0000

    initial commit

---
+++ LangConcept/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace LangConcept.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("LangConcept")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("LangConcept")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("7e589a5b-07d3-440d-875e-e49322863ecc")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ LangConcept/LangConcept01_3.fsx
@@ -0,0 +1,30 @@
+﻿[<AutoOpen>]
+module LangConcept01_3 = 
+
+    type Expr = 
+        | CI of int
+        | Prim of string * Expr * Expr
+
+    let rec Eval (e:Expr) : int = 
+        match e with 
+        | CI x -> x
+        | Prim ("+",e1,e2) -> (+) (Eval e1) (Eval e2)
+        | Prim ("-",e1,e2) -> (-) (Eval e1) (Eval e2)
+        | Prim ("*",e1,e2) -> (*) (Eval e1) (Eval e2)
+        | Prim _ -> failwith "primitive not recognised"
+
+let e1  = CI 1
+let e2  = Prim ("+" , CI 1, CI 2)
+let e3  = Prim ("-" , CI 1, CI 2)
+let e4  = Prim ("*" , CI 1, CI 2)
+
+Eval e1
+Eval e2
+Eval e3
+Eval e4
+
+
+
+
+
+
